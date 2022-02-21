0b99cef66a224ab1e0ab386ddac4cdce71db6e23
dimitermusic <dimiteryyordanov@gmail.com>
2022-02-21 15:23:28 -0800

    sync back to false/rebuilding db

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(routes)
 
-sequelize.sync({ force: true }).then(function () {
+sequelize.sync({ force: false }).then(function () {
     app.listen(PORT, function () {
         console.log('Listening at' + PORT);
     })
