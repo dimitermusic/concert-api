214f0bb37f8fdf1eea941e0da763a04ae525ea83
dimitermusic <dimiteryyordanov@gmail.com>
2022-02-21 13:01:48 -0800

    added Access-Control-Allow-Origin headers

--- index.js
+++ index.js
@@ -10,14 +10,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 app.use(routes)
-app.use(cors(
-    {
-        "origin": "*",
-        "methods": "GET",
-        "preflightContinue": false,
-        "optionsSuccessStatus": 204
-    }
-));
+app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
 
 sequelize.sync({ force: false }).then(function () {
