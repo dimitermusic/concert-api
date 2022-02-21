84c57044a3e0c148913522381c80be96e4f66f14
dimitermusic <dimiteryyordanov@gmail.com>
2022-02-21 11:44:27 -0800

    updated cors policy

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const sequelize = require('./config/connection');
 const routes = require('./controllers')
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 4747;
 
-app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 app.use(routes)
+app.use(cors({
+    origin: '*'
+}));
 
 sequelize.sync({ force: false }).then(function () {
     app.listen(PORT, function () {
