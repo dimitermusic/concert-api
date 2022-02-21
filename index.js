84d3c7b4ce4d076c77f0a6410f7f2d777ea7acbb
dimitermusic <dimiteryyordanov@gmail.com>
2022-02-21 13:32:54 -0800

    updated cors middleware

--- index.js
+++ index.js
@@ -6,19 +6,15 @@ const cors = require('cors');
 const PORT = process.env.PORT || 4747;
 
 
+app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 app.use(routes)
-app.use(cors());
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-});
 
 sequelize.sync({ force: false }).then(function () {
     app.listen(PORT, function () {
