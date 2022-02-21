5a5c1d57bc42104d7d5acd5b148135f4da657d62
dimitermusic <dimiteryyordanov@gmail.com>
2022-02-21 13:56:12 -0800

    updated wildcard fallback routes message

--- controllers/index.js
+++ controllers/index.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
-router.get("/", (req, res) => {
-    res.send("Add '/api/concerts' to this url to view Dimiter's concert API!")
+router.get("*", (req, res) => {
+    res.send(`Visit <a href="https://dimiter-concert-api.herokuapp.com/api/concerts">https://dimiter-concert-api.herokuapp.com/api/concerts</a> to view Dimiter's concert API.`)
 })
 
 module.exports = router;
