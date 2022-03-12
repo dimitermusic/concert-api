3d3be5985f7b42b1b5917237130b16b0822d2653
dimitermusic <dimiteryyordanov@gmail.com>
2022-03-11 21:02:56 -0800

    refactor

--- public/js/index.js
+++ public/js/index.js
@@ -4,18 +4,10 @@ console.log('hello world');
 // Dynamically set copyright year to automatically update
 const handleCopyrightMessage = () => {
 
-    let copyrightText = document.getElementById('copyright-text')
+    let copyrightText = document.getElementById('copyright-text');
     let thisYear = new Date().toDateString().slice(11);
-    let githubLink = document.createElement('a')
 
-    copyrightText.textContent = `© ${thisYear} Dimiter Yordanov. All Rights Reserved. Powered by `;
-
-    // Add link to dynamically created copyright text
-    githubLink.textContent = 'Dimiter Yordanov.'
-    githubLink.href = 'https://www.github.com/dimitermusic'
-    githubLink.target = '_blank'
-    githubLink.classList.add('github-link')
-    copyrightText.appendChild(githubLink)
+    copyrightText.prepend(`© ${thisYear} Dimiter Yordanov. All Rights Reserved. Powered by `);
 
 
 }
