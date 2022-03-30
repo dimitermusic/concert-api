6354fc33651c7c6c89186f09ddfaedb595661008
Dimiter Yordanov <dimiteryyordanov@gmail.com>
2022-03-29 23:58:30 -0700

    added security to app

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const concertRoutes = require("./concertRoutes");
 const path = require("path");
 
-router.use(`APIKEY=${process.env.API_KEY}/concerts`, concertRoutes);
+router.use(`/APIKEY=${process.env.API_KEY}/concerts`, concertRoutes);
 
 router.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/../public/index.html"));
--- public/js/index.js
+++ public/js/index.js
@@ -2,18 +2,12 @@
 console.log("hello world");
 
 // Dynamically set copyright year to automatically update
-const handleCopyrightMessage = () => {
+function handleCopyrightMessage() {
   let copyrightText = document.getElementById("copyright-text");
   let thisYear = new Date().toDateString().slice(11);
   copyrightText.prepend(
     `© ${thisYear} Dimiter Yordanov. All Rights Reserved. Powered by `
   );
-};
-
-const handleApiKey = () => {
-  let apiBtn = document.getElementById("apiBtn");
-  apiBtn.href = `https://api.dimitermusic.com/APIKEY=${process.env.API_KEY}/concerts`;
-};
+}
 
 handleCopyrightMessage();
-handleApiKey;
