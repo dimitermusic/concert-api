af375880174ed256423d999cf301ad246d17ef9c
Dimiter Yordanov <dimiteryyordanov@gmail.com>
2022-03-29 23:08:23 -0700

    updated concert api button in ui

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,19 @@
 // Confirm file is linked to html
-console.log('hello world');
+console.log("hello world");
 
 // Dynamically set copyright year to automatically update
 const handleCopyrightMessage = () => {
+  let copyrightText = document.getElementById("copyright-text");
+  let thisYear = new Date().toDateString().slice(11);
+  copyrightText.prepend(
+    `© ${thisYear} Dimiter Yordanov. All Rights Reserved. Powered by `
+  );
+};
 
-    let copyrightText = document.getElementById('copyright-text');
-    let thisYear = new Date().toDateString().slice(11);
-
-    copyrightText.prepend(`© ${thisYear} Dimiter Yordanov. All Rights Reserved. Powered by `);
-
-
-}
+const handleApiKey = () => {
+  let apiBtn = document.getElementById("apiBtn");
+  apiBtn.href = `https://api.dimitermusic.com/APIKEY=${process.env.API_KEY}/concerts`;
+};
 
 handleCopyrightMessage();
+handleApiKey;
