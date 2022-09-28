a08361a194cc969b8736409c545abfb3f18e4c55
dimitermusic <dimiteryyordanov@gmail.com>
2022-09-27 23:33:09 -0700

    updating from jawsdb to aws rds

--- config/connection.js
+++ config/connection.js
@@ -3,19 +3,20 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: 'localhost',
-            dialect: 'mysql',
-            port: 3306
-        }
-    );
-}
+sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: 3306,
+        dialect: 'mysql',
+        dialectOptions: {
+            ssl:'Amazon RDS'
+        },
+        pool: { maxConnections: 5, maxIdleTime: 30},
+        language: 'en'
+    }
+);
 
 module.exports = sequelize;
