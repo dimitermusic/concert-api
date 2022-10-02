379eb28c08cc6d6fff01f3a58a4d45a94e536e0d
dimitermusic <dimiteryyordanov@gmail.com>
2022-10-02 13:14:33 -0700

    setting sequelize sync force to false

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(routes)
 
-sequelize.sync({ force: true })
+sequelize.sync({ force: false })
   .then(function () {
     app.listen(PORT, function () {
         console.log(`Listening at http://localhost:${PORT}`);
